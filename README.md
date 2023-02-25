831fe68daa68440d674647eac2246c30cda26584
Easton Man <manyang.me@outlook.com>
2023-02-25 15:01:23 +0800

    docs: update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # linux101-4
+
+HITsz LUG Linux101 Ta1k 0x4
+
+[Bilibili Video](https://www.bilibili.com/video/BV1E8411e7hF)
--- code/signal/main.c
+++ code/signal/main.c
@@ -2,7 +2,7 @@
  * @Author: Easton Man manyang.me@outlook.com
  * @Date: 2022-10-15 19:13:41
  * @LastEditors: Easton Man manyang.me@outlook.com
- * @LastEditTime: 2022-10-15 19:16:02
+ * @LastEditTime: 2022-10-16 18:27:26
  * @FilePath: /linux101-4/code/signal/main.c
  * @Description:
  */
@@ -10,10 +10,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static void signal_handler(int sig) { printf("Received signal %d\n", sig); }
+static void signal_handler(int sig) {
+  printf("Received signal %d\n", sig);
+  fflush(stdout);
+  alarm(1);
+}
 
 int main(int argc, char **argv) {
   signal(SIGUSR2, signal_handler);
+  signal(SIGALRM, signal_handler);
+  alarm(1);
   while (1) {
   }
   return 0;
